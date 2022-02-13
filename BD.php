1b5e8697467a864e31ed6947d2ee23dac670e975
lemonsh1t <99617435+lemonsh1t@users.noreply.github.com>
2022-02-13 08:59:15 -1200

    Add files via upload

---
+++ BD.php
@@ -0,0 +1,26 @@
+<?php
+//conexión con la base de datos y el servidor
+$link = mysqli_connect("localhost","root","") or die ("<h2>No se encuentra el servidor</h2>");
+
+$db = "usuarios";
+mysqli_select_db($link, $db) or die ("<h2>Error de conexion con la BD</h2>");
+
+//obtenemos los valores del formulario
+$nombre = $_POST['nombres'];
+$apellido = $_POST['apellidos'];
+$edad = $_POST['edad'];
+$genero = $_POST['genero'];
+$prepa = $_POST['preparatoria'];
+
+$sql = "INSERT INTO datoa(nombres,apellidos,edad,genero,preparatoria,) VALUES ('$nombre','$apellido','$edad','$genero','$prepa')";
+$sqlrun = mysqli_query($link,$sql);
+
+if($sqlrun){
+	echo "Información enviada";
+}
+else
+{
+	echo "NO se envío la información.";
+}
+
+?>
---
+++ xd.cpp
@@ -0,0 +1,67 @@
+* {
+	margin: 0;
+	padding: 0;
+	box-sizing: border-box;
+}
+body {
+	background-color: #BCAFBD;
+}
+.form-registro {
+	width: 400px;
+	background: #F6CFCA;
+	padding: 30px;
+	margin: auto;
+	margin-top: 100px;
+	border-radius: 4px;
+	font-family: calibri;
+	color: white;
+	box-shadow: 7px 13px 37px #756477;
+}
+.form-registro h4 {
+	font-size: 22px;
+	margin-bottom: 20px;
+
+}
+.controls {
+	width: 100%;
+	background: #FFE8E5;
+	padding: 10px;
+	border-radius: 4px;
+	margin-bottom: 16px;
+	border: 1px solid #F7F7F7;
+	font-family: calibri;
+	font-size: 18px;
+}
+.form-registro p {
+	height: 40px;
+	text-align: center;
+	font-size: 18px;
+	line-height: 40px;
+}
+
+.form-registro a {
+	color: #F7F7F7;
+	text-decoration: none;
+}
+.form-registro a:hover {
+	color: white;
+	text-decoration: none;
+} 
+.form-registro .botons {
+	width: 100%;
+	background: #F9F6E4;
+	border: none;
+	padding: 12px;
+	color: #CCA4A6;
+	margin: 16px 0;
+	font-size: 16px;
+  }
+.form-registro .type  {
+width: 100%;
+	background: #F9F6E4;
+	border: none;
+	padding: 12px;
+	color: #CCA4A6;
+	margin: 16px 0;
+	font-size: 16px;
+}
